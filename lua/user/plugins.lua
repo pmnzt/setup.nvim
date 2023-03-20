7412d65a1d916536f69c72fe3181958db8acbb59
pmnzt <vponsve@gmail.com>
2023-03-21 00:07:36 +0300

    first commit

---
+++ lua/user/plugins.lua
@@ -0,0 +1,16 @@
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+return packer.startup(function(use)
+  use "wbthomason/packer.nvim"
+  use "nvim-lualine/lualine.nvim" 
+  use "windwp/nvim-autopairs"
+
+  -- git
+  use "lewis6991/gitsigns.nvim"      
+
+  -- colorschemes
+ use "morhetz/gruvbox"
+end)
